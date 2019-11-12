090b369776ad3bf65f7c61f683c0d86635b398d0
Sean Bastille <sean.bastille@verizonmedia.com>
2019-11-12 17:25:44 -0500

    Updating table to match prior update to recommendations output

--- schema/02_tbl_reserved_instances_recommendations.sql
+++ schema/02_tbl_reserved_instances_recommendations.sql
@@ -4,18 +4,17 @@ SET client_min_messages TO warning;
 
 CREATE TABLE reserved_instances_recommendations
 (
-    "Account Id"          character(19)               NOT NULL,
+    "Account ID"          character(12)               NOT NULL,
     "Scope"               character varying(16)       NOT NULL, -- [Region, Availabilty Zone]
     "Region"              character varying(20)       NOT NULL, -- ap-northeast-3 + 6
-    "External AZ"         character varying(21)       NULL,     -- region + 1
-    "Offering Class"      character varying(11)       NOT NULL, -- [classic, convertible]
-    "Quantity"            smallint                    NOT NULL,
-    "Term"                smallint                    NOT NULL, -- [12, 36]
     "Instance Type"       character varying(15)       NOT NULL, -- r5ad.24xlarge + 2
-    "Product Description" character varying(7)        NOT NULL, -- [Linux, RHEL, SUSE, Windows]
-    "RI Type"             character varying(15)       NOT NULL, -- [All Upfront, Partial Upfront, No Upfront]
+    "Operating System"    character varying(23)       NOT NULL, -- [Linux/UNIX (Amazon VPC), RHEL, SUSE, Windows]
     "Tenancy"             character varying(9)        NOT NULL, -- [Dedicated, Host, Shared]
-    accountid             character(12)               NOT NULL,
+    "Offering Class"      character varying(11)       NOT NULL, -- [classic, convertible]
+    "Payment Type"        character varying(15)       NOT NULL, -- [All Upfront, Partial Upfront, No Upfront]
+    "Term"                smallint                    NOT NULL, -- [12, 36]
+    "Quantity"            smallint                    NOT NULL,
+    accountid             character(19)               NOT NULL,
     accountname           character varying(64)       NOT NULL,
     family                character varying(5)        NOT NULL, -- r5ad + 1
     units                 double precision            NOT NULL,
