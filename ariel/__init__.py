52b0468910619dced31fe6d88e839c153221286d
Sean Bastille <sean.bastille@verizonmedia.com>
2019-11-12 12:18:13 -0500

    Version update

--- ariel/__init__.py
+++ ariel/__init__.py
@@ -2,7 +2,7 @@
 # Licensed under the terms of the Apache License, Version 2.0. See LICENSE file for terms.
 __all__ = ['generate_reports', 'get_account_instance_summary', 'get_account_names', 'get_ec2_pricing', 'get_locations',
            'get_reserved_instances', 'get_unlimited_summary', 'get_unused_box_summary', 'utils', 'LOGGER']
-__version__='2.0.7'
+__version__='2.0.8'
 
 import logging
 LOG_LEVEL = logging.INFO
